d8dd4acbcd735922ee8c88a80f74dfe66081349f
penprocess <149070900+penprocess@users.noreply.github.com>
2023-10-27 14:23:01 +0530

    Update test.py

--- pinecone/test.py
+++ pinecone/test.py
@@ -1,4 +1,4 @@
 from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader
-loader =  OnlinePDFLoader("https://wolfpaulus.com/wp-content/uploads/2017/05/field-guide-to-data-science.pdf")
+loader =  OnlinePDFLoader("")
 data = loader.load()
 print(data)
